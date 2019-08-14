d85a0f032bbe7fafddc855145246f1dd9a950b31
chris <cglerup@gmail.com>
2019-08-13 19:09:51 -0500

    updated login page

--- craft-brewery/client/src/views/examples/LoginPage.js
+++ craft-brewery/client/src/views/examples/LoginPage.js
@@ -9,27 +9,50 @@ import {
   Input,
   Container,
   Row,
+  NavItem,
+  NavLink,
   Col
 } from "reactstrap";
 
 // core components
-import MultiDropdownNavbar from "components/Navbars/MultiDropdownNavbar.js";
+import axios from 'axios';
+export default class RegisterPage extends React.Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-function LoginPage() {
-  document.documentElement.classList.remove("nav-open");
-  React.useEffect(() => {
-    document.body.classList.add("login-page");
+      email:'',
+      confirm:''
+
+
+    }
+  }
+  handleRegister = (event) => {
+    // event.preventDefault();
+    axios.get('/profiles',this.state).then(res => {
+      console.log(res);
+    })
+  }
+  handleChange = (event) => {
+    const { name, value } = event.target || event.srcElement;
+    this.setState({
+      [name]: value
+    });
+  }
+  componentDidMount() {
+    document.documentElement.classList.remove("nav-open");
+    document.body.classList.add("register-page");
     document.body.classList.add("full-screen");
     window.scrollTo(0, 0);
     document.body.scrollTop = 0;
     return function cleanup() {
-      document.body.classList.remove("login-page");
+      document.body.classList.remove("register-page");
       document.body.classList.remove("full-screen");
     };
-  });
+  }
+ render(){
   return (
     <>
-      <MultiDropdownNavbar />
       <div className="wrapper">
         <div
           className="page-header"
@@ -44,24 +67,18 @@ function LoginPage() {
               <Col className="ml-auto mr-auto" lg="4" md="6" sm="6">
                 <Card className="card-register">
                   <CardTitle tag="h3">Welcome</CardTitle>
-                  <Form className="register-form">
-                    <label>Email</label>
-                    <Input
-                      className="no-border"
-                      placeholder="Email"
-                      type="email"
-                    />
-                    <label>Password</label>
-                    <Input
-                      className="no-border"
-                      placeholder="Password"
-                      type="password"
-                    />
-                    <Button block className="btn-round" color="danger">
+                  <Form className="register-form" method="GET" action="landing-page">
+                  <Input onChange={this.handleChange} placeholder="Email" type="text" name="email" />
+                  <Input onChange={this.handleChange} placeholder="Password" type="password" name='password' />
+                  <Button onClick={this.handleRegister} block className="btn-round" color="default">
                       Login
                     </Button>
                   </Form>
-
+                  <div className="login">
+                <NavItem>
+                <NavLink href="/register-page/">Dont have an account? register Now</NavLink>
+                </NavItem>                
+                  </div>
                 </Card>
               </Col>
             </Row>
@@ -77,5 +94,5 @@ function LoginPage() {
     </>
   );
 }
+}
 
-export default LoginPage;
